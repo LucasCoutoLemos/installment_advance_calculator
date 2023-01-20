7ee162fe1136be9db22768c52642b3cf60ffa071
Lucas do Couto Lemos <lemos120402@gmail.com>
2023-01-20 09:42:22 -0300

    Feat: Providers and services

---
+++ src/interfaces/calculator.tsx
@@ -0,0 +1,13 @@
+export interface IForm extends IDays {
+  amount: number;
+  installments: number;
+  mdr: number;
+  days?: number[];
+}
+
+export interface IDays {
+  '1': number;
+  '15': number;
+  '30': number;
+  '90': number;
+}
