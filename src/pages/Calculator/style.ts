e7078f6d7652211e4ba31f1fc913777f18412318
Lucas do Couto Lemos <lemos120402@gmail.com>
2023-01-20 22:28:27 -0300

    Feat: Calculator page

---
+++ src/pages/Calculator/style.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const StyledCalculator = styled.div`
+  @media screen and (min-width: 1024px) {
+    display: flex;
+    justify-content: center;
+    margin-top: 3rem;
+    width: 50%;
+    margin-left: 20rem;
+    box-shadow: 3px 3px 20px -8px rgb(170 170 170);
+  }
+`;
