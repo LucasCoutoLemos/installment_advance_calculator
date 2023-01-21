3b46fabd52a5018ca2978cb61db71b753d1cc9b8
Lucas do Couto Lemos <lemos120402@gmail.com>
2023-01-20 22:23:09 -0300

    Feat: Components form and request

---
+++ src/components/Form/index.tsx
@@ -0,0 +1,47 @@
+import { StyledContainerCalculator } from "./style";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { formSchema } from "../../validators/schema";
+import { IForm } from "../../interfaces/calculator";
+import { useContext } from "react";
+import { CalculatorContext } from "../../providers/Calculator";
+import React from "react";
+
+export const Form = () => {
+    const { handleSubmitCalculatorForm } = useContext(CalculatorContext);
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<IForm>({
+        resolver: yupResolver(formSchema),
+    });
+
+    return (
+        <StyledContainerCalculator>
+            <div className="title">
+                <h2>Simule sua Antecipação</h2>
+            </div>
+            <form onSubmit={handleSubmit(handleSubmitCalculatorForm)}>
+                <div className="sectionOne">
+                    <label htmlFor="amount">Informe o valor da venda *</label>
+                    <input
+                        type="number"
+                        placeholder="R$"
+                        min="1000"
+                        {...register("amount")}
+                    />
+                    <p>{errors.amount?.message}</p>
+                    <label htmlFor="installments">Em quantas parcelas *</label>
+                    <input type="number" min="1" max="12" {...register("installments")} />
+                    <span>Máximo de 12 parcelas</span>
+                    <p>{errors.installments?.message}</p>
+                    <label htmlFor="mdr">Informe o percentual de MDR *</label>
+                    <input type="number" min="1" {...register("mdr")} />
+                    <p>{errors.mdr?.message}</p>
+                    <button className="btn">Calcular</button>
+                </div>
+            </form>
+        </StyledContainerCalculator>
+    );
+};
---
+++ src/components/Response/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { useContext } from "react";
+import { CalculatorContext } from "../../providers/Calculator";
+import { CalculatorResponseContainer } from "./style";
+
+export const Response = () => {
+    const { day } = useContext(CalculatorContext);
+    return (
+        <CalculatorResponseContainer>
+            <div className="sectionTwo">
+                <h3>VOCÊ RECEBERÁ: </h3>
+                <hr></hr>
+                <p>Amanhã: R$ {day['1']}</p>
+                <p>Em 15 dias: R$ {day['15']}</p>
+                <p>Em 30 dias: R$ {day['30']}</p>
+                <p>Em 90 dias: R$ {day['90']}</p>
+            </div>
+        </CalculatorResponseContainer>
+    );
+};
